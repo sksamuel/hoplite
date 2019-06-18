e73b9c701dd225e17de231956c29c4d8c17c2592
sksamuel <sam@sksamuel.com>
2019-06-17 21:58:31 -0500

    Added prefix preprocessor convenience class

--- hoplite-core/src/main/kotlin/com/sksamuel/hoplite/preprocessor/Preprocessor.kt
+++ hoplite-core/src/main/kotlin/com/sksamuel/hoplite/preprocessor/Preprocessor.kt
@@ -15,3 +15,9 @@ object EnvVarPreprocessor : Preprocessor {
     return regex.replace(value) { it.groupValues[1] }
   }
 }
+
+abstract class PrefixProcessor(private val prefix: String) : Preprocessor {
+  abstract fun handle(value: String): String
+  override fun process(value: String): String =
+      if (value.startsWith(prefix)) handle(value) else value
+}
