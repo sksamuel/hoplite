085b86ca935a3b2648012655b367b1e2a0fb57f1
sksamuel <sam@sksamuel.com>
2019-07-24 21:50:14 -0500

    Fixed tests

--- hoplite-json/src/test/kotlin/com/sksamuel/hoplite/json/JsonTest.kt
+++ hoplite-json/src/test/kotlin/com/sksamuel/hoplite/json/JsonTest.kt
@@ -18,12 +18,12 @@ class JsonTest : FunSpec() {
       Json.load(javaClass.getResourceAsStream("/basic.json")) shouldBe
           MapValue(
               mapOf(
-                  "a" to StringValue(value = "hello", pos = Pos.LineColPos(line = 2, col = 15)),
-                  "b" to LongValue(value = 1, pos = Pos.LineColPos(line = 3, col = 9)),
-                  "c" to BooleanValue(value = true, pos = Pos.LineColPos(line = 4, col = 12)),
-                  "d" to DoubleValue(value = 2.3, pos = Pos.LineColPos(line = 5, col = 11))
+                  "a" to StringValue(value = "hello", pos = LineColPos(line = 2, col = 15)),
+                  "b" to LongValue(value = 1, pos = LineColPos(line = 3, col = 9)),
+                  "c" to BooleanValue(value = true, pos = LineColPos(line = 4, col = 12)),
+                  "d" to DoubleValue(value = 2.3, pos = LineColPos(line = 5, col = 11))
               ),
-              pos = Pos.RangePos(start = -1, end = -1)
+              pos = LineColPos(line = 1, col = 2)
           )
     }
 
@@ -31,17 +31,17 @@ class JsonTest : FunSpec() {
       Json.load(javaClass.getResourceAsStream("/nested_basic_arrays.json")) shouldBe
           MapValue(
               mapOf(
-                  "a" to StringValue(value = "hello", pos = Pos.LineColPos(line = 2, col = 15)),
+                  "a" to StringValue(value = "hello", pos = LineColPos(line = 2, col = 15)),
                   "b" to ListValue(
                       values = listOf(
-                          StringValue(value = "x", pos = Pos.LineColPos(line = 4, col = 8)),
-                          StringValue(value = "y", pos = Pos.LineColPos(line = 5, col = 8)),
-                          StringValue(value = "z", pos = Pos.LineColPos(line = 6, col = 8))
+                          StringValue(value = "x", pos = LineColPos(line = 4, col = 8)),
+                          StringValue(value = "y", pos = LineColPos(line = 5, col = 8)),
+                          StringValue(value = "z", pos = LineColPos(line = 6, col = 8))
                       ),
-                      pos = Pos.RangePos(start = -1, end = -1)
+                      pos = LineColPos(line = 3, col = 9)
                   )
               ),
-              pos = Pos.RangePos(start = -1, end = -1)
+              pos = LineColPos(line = 1, col = 2)
           )
     }
 
@@ -49,7 +49,7 @@ class JsonTest : FunSpec() {
       Json.load(javaClass.getResourceAsStream("/nested_container_arrays.json")) shouldBe
           MapValue(
               mapOf(
-                  "a" to StringValue(value = "hello", pos = Pos.LineColPos(line = 2, col = 15)),
+                  "a" to StringValue(value = "hello", pos = LineColPos(line = 2, col = 15)),
                   "b" to ListValue(
                       values = listOf(
                           MapValue(
@@ -57,20 +57,20 @@ class JsonTest : FunSpec() {
                                   "c" to StringValue(value = "hello", pos = LineColPos(line = 5, col = 19)),
                                   "d" to BooleanValue(value = true, pos = LineColPos(line = 6, col = 16))
                               ),
-                              pos = Pos.RangePos(start = -1, end = -1)
+                              pos = LineColPos(line = 4, col = 6)
                           ),
                           MapValue(
                               map = mapOf(
                                   "e" to DoubleValue(value = 1.4, pos = LineColPos(line = 9, col = 15)),
                                   "f" to LongValue(value = 4, pos = LineColPos(line = 10, col = 13))
                               ),
-                              pos = Pos.RangePos(start = -1, end = -1)
+                              pos = LineColPos(line = 8, col = 6)
                           )
                       ),
-                      pos = Pos.RangePos(start = -1, end = -1)
+                      pos = LineColPos(line = 3, col = 9)
                   )
               ),
-              pos = Pos.RangePos(start = -1, end = -1)
+              pos = LineColPos(line = 1, col = 2)
           )
     }
   }
