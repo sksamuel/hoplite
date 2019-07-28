c8dee59083b3c2693a71d98b0e195325dd79b419
sksamuel <sam@sksamuel.com>
2019-07-28 01:16:48 -0500

    Fixed json tests

--- hoplite-json/src/test/kotlin/com/sksamuel/hoplite/json/JsonTest.kt
+++ hoplite-json/src/test/kotlin/com/sksamuel/hoplite/json/JsonTest.kt
@@ -17,13 +17,16 @@ class JsonTest : FunSpec() {
       JsonParser().load(javaClass.getResourceAsStream("/basic.json"), source = "a.json") shouldBe
         MapNode(
           mapOf(
-            "a" to StringNode(value = "hello", pos = LineColPos(line = 2, col = 15, source = "a.json"), dotpath = ""),
-            "b" to LongNode(value = 1, pos = LineColPos(line = 3, col = 3, source = "a.json"), dotpath = ""),
-            "c" to BooleanNode(value = true, pos = LineColPos(line = 4, col = 12, source = "a.json"), dotpath = ""),
-            "d" to DoubleNode(value = 2.3, pos = LineColPos(line = 5, col = 11, source = "a.json"), dotpath = "")
+            "a" to StringNode(value = "hello", pos = LineColPos(line = 2, col = 15, source = "a.json"),
+              dotpath = "<root>.a"),
+            "b" to LongNode(value = 1, pos = LineColPos(line = 3, col = 9, source = "a.json"), dotpath = "<root>.b"),
+            "c" to BooleanNode(value = true, pos = LineColPos(line = 4, col = 12, source = "a.json"),
+              dotpath = "<root>.c"),
+            "d" to DoubleNode(value = 2.3, pos = LineColPos(line = 5, col = 11, source = "a.json"),
+              dotpath = "<root>.d")
           ),
           pos = LineColPos(line = 1, col = 2, source = "a.json"),
-          dotpath = ""
+          dotpath = "<root>"
         )
     }
 
@@ -31,19 +34,21 @@ class JsonTest : FunSpec() {
       JsonParser().load(javaClass.getResourceAsStream("/nested_basic_arrays.json"), source = "a.json") shouldBe
         MapNode(
           mapOf(
-            "a" to StringNode(value = "hello", pos = LineColPos(line = 2, col = 15, source = "a.json"), dotpath = ""),
+            "a" to StringNode(value = "hello",
+              pos = LineColPos(line = 2, col = 15, source = "a.json"),
+              dotpath = "<root>.a"),
             "b" to ListNode(
               elements = listOf(
-                StringNode(value = "x", pos = LineColPos(line = 4, col = 8, source = "a.json"), dotpath = ""),
-                StringNode(value = "y", pos = LineColPos(line = 5, col = 8, source = "a.json"), dotpath = ""),
-                StringNode(value = "z", pos = LineColPos(line = 6, col = 8, source = "a.json"), dotpath = "")
+                StringNode(value = "x", pos = LineColPos(line = 4, col = 8, source = "a.json"), dotpath = "<root>.b[0]"),
+                StringNode(value = "y", pos = LineColPos(line = 5, col = 8, source = "a.json"), dotpath = "<root>.b[1]"),
+                StringNode(value = "z", pos = LineColPos(line = 6, col = 8, source = "a.json"), dotpath = "<root>.b[2]")
               ),
               pos = LineColPos(line = 3, col = 9, source = "a.json"),
-              dotpath = ""
+              dotpath = "<root>.b"
             )
           ),
           pos = LineColPos(line = 1, col = 2, source = "a.json"),
-          dotpath = ""
+          dotpath = "<root>"
           )
     }
 
@@ -53,40 +58,36 @@ class JsonTest : FunSpec() {
           mapOf(
             "a" to StringNode(value = "hello",
               pos = LineColPos(line = 2, col = 15, source = "a.json"),
-              dotpath = ""),
+              dotpath = "<root>.a"),
             "b" to ListNode(
               elements = listOf(
                 MapNode(
                   map = mapOf(
-                    "c" to StringNode(value = "hello",
-                      pos = LineColPos(line = 5, col = 19, source = "a.json"),
-                      dotpath = ""),
-                    "d" to BooleanNode(value = true,
-                      pos = LineColPos(line = 6, col = 16, source = "a.json"),
-                      dotpath = "")
+                    "c" to StringNode(value = "hello", pos = LineColPos(line = 5, col = 19, source = "a.json"),
+                      dotpath = "<root>.b[0].c"),
+                    "d" to BooleanNode(value = true, pos = LineColPos(line = 6, col = 16, source = "a.json"),
+                      dotpath = "<root>.b[0].d")
                   ),
                   pos = LineColPos(line = 4, col = 6, source = "a.json"),
-                  dotpath = ""
+                  dotpath = "<root>.b[0]"
                 ),
                 MapNode(
                   map = mapOf(
-                    "e" to DoubleNode(value = 1.4,
-                      pos = LineColPos(line = 9, col = 15, source = "a.json"),
-                      dotpath = ""),
-                    "f" to LongNode(value = 4,
-                      pos = LineColPos(line = 10, col = 13, source = "a.json"),
-                      dotpath = "")
+                    "e" to DoubleNode(value = 1.4, pos = LineColPos(line = 9, col = 15, source = "a.json"),
+                      dotpath = "<root>.b[1].e"),
+                    "f" to LongNode(value = 4, pos = LineColPos(line = 10, col = 13, source = "a.json"),
+                      dotpath = "<root>.b[1].f")
                   ),
                   pos = LineColPos(line = 8, col = 6, source = "a.json"),
-                  dotpath = ""
+                  dotpath = "<root>.b[1]"
                 )
               ),
               pos = LineColPos(line = 3, col = 9, source = "a.json"),
-              dotpath = ""
+              dotpath = "<root>.b"
             )
           ),
           pos = LineColPos(line = 1, col = 2, source = "a.json"),
-          dotpath = ""
+          dotpath = "<root>"
         )
     }
   }
