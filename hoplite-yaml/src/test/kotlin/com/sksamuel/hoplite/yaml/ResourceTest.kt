03bce5060ce289445bdea2d9a892fd3d9ebe037d
sksamuel <sam@sksamuel.com>
2019-07-29 20:38:59 -0500

    Fixed test

--- hoplite-yaml/src/test/kotlin/com/sksamuel/hoplite/yaml/ResourceTest.kt
+++ hoplite-yaml/src/test/kotlin/com/sksamuel/hoplite/yaml/ResourceTest.kt
@@ -13,7 +13,7 @@ class ResourceTest : FunSpec({
   test("return failure for missing resource") {
     data class Foo(val a: String)
     ConfigLoader().loadConfig<Foo>("/missing.yml").shouldBeInvalid {
-      it.e shouldBe NonEmptyList.just(ConfigFailure("Could not find resource /missing.yml"))
+      it.e shouldBe NonEmptyList.just(ConfigFailure.UnknownSource("/missing.yml"))
     }
   }
 
