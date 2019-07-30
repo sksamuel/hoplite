5927b10990b6cffd65c8fa609da54bfb253a5942
fagossa <fagossa@gmail.com>
2019-07-30 14:45:07 +0200

    Adds JavaUtilDateDecoder and YearDecoder
    This commit does the following issues:
    * https://github.com/sksamuel/hoplite/issues/41
    * https://github.com/sksamuel/hoplite/issues/31

--- hoplite-core/src/test/kotlin/com/sksamuel/hoplite/decoder/DataClassDecoderTest.kt
+++ hoplite-core/src/test/kotlin/com/sksamuel/hoplite/decoder/DataClassDecoderTest.kt
@@ -9,6 +9,9 @@ import com.sksamuel.hoplite.Pos
 import com.sksamuel.hoplite.StringNode
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.StringSpec
+import java.time.LocalDate
+import java.time.Year
+import java.time.ZoneOffset
 import kotlin.reflect.full.createType
 
 class DataClassDecoderTest : StringSpec() {
@@ -57,6 +60,25 @@ class DataClassDecoderTest : StringSpec() {
       )
       DataClassDecoder().decode(node, Foo::class.createType(), defaultDecoderRegistry()) shouldBe
         Foo("hello", 123, true).valid()
+    }
+
+    "support Date types" {
+      data class Foo(val a: Year, val b: java.util.Date)
+
+      val millis = LocalDate.parse("2011-08-17")
+        .atStartOfDay()
+        .toInstant(ZoneOffset.UTC)
+      val expectedDate = java.util.Date.from(millis)
+
+      val node = MapNode(
+        mapOf(
+          "a" to StringNode("1991", Pos.NoPos, dotpath = ""),
+          "b" to LongNode(millis.toEpochMilli(), Pos.NoPos, dotpath = "")
+        ),
+        Pos.NoPos, dotpath = ""
+      )
+      DataClassDecoder().decode(node, Foo::class.createType(), defaultDecoderRegistry()) shouldBe
+        Foo(Year.of(1991), expectedDate).valid()
     }
   }
 }
