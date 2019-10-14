1e594cfcf9c38ea320754dab84295b20bb6ea51f
sksamuel <sam@sksamuel.com>
2019-10-13 22:41:34 -0500

    Updated triple decoder test

--- hoplite-core/src/test/kotlin/com/sksamuel/hoplite/decoder/TripleDecoderTest.kt
+++ hoplite-core/src/test/kotlin/com/sksamuel/hoplite/decoder/TripleDecoderTest.kt
@@ -4,13 +4,12 @@ import com.sksamuel.hoplite.ConfigLoader
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.StringSpec
 
-class TripleDecoderTest : StringSpec() {
-  init {
+class TripleDecoderTest : StringSpec({
+
     "triple should be decoded from string with 3 fields" {
       data class Test(val a: Triple<String, String, Boolean>, val b: Triple<String, Long, Int>)
 
       val config = ConfigLoader().loadConfigOrThrow<Test>("/test_triple.props")
       config shouldBe Test(Triple("hello", "world", true), Triple("5", 4L, 3))
     }
-  }
-}
+})
