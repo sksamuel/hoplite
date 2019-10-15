6e26bebd7f745a3adde879615bf21e207b6e44b0
sksamuel <sam@sksamuel.com>
2019-10-14 21:29:27 -0500

    Moved decoder registry to it's own file

--- hoplite-core/src/main/kotlin/com/sksamuel/hoplite/decoder/Decoder.kt
+++ hoplite-core/src/main/kotlin/com/sksamuel/hoplite/decoder/Decoder.kt
@@ -3,55 +3,15 @@ package com.sksamuel.hoplite.decoder
 import arrow.core.Valid
 import arrow.core.Validated
 import arrow.core.invalid
-import arrow.core.valid
 import com.sksamuel.hoplite.ConfigFailure
 import com.sksamuel.hoplite.ConfigResult
 import com.sksamuel.hoplite.DecoderContext
 import com.sksamuel.hoplite.MapNode
-import com.sksamuel.hoplite.NullValue
 import com.sksamuel.hoplite.Node
+import com.sksamuel.hoplite.NullValue
 import com.sksamuel.hoplite.Undefined
-import java.util.*
-import kotlin.reflect.KClass
 import kotlin.reflect.KType
 import kotlin.reflect.full.createType
-
-inline fun <reified T : Any> DecoderRegistry.decoder(): ConfigResult<Decoder<T>> = decoder(T::class)
-
-/**
- * An immutable registry for providing instances of a [Decoder] for a given type.
- */
-interface DecoderRegistry {
-
-  fun <T : Any> decoder(t: KClass<T>): ConfigResult<Decoder<T>>
-  fun decoder(type: KType): ConfigResult<Decoder<*>>
-  fun register(decoder: Decoder<*>): DecoderRegistry
-  val size: Int
-
-  companion object {
-    val zero: DecoderRegistry = DefaultDecoderRegistry(emptyList())
-  }
-}
-
-@Suppress("UNCHECKED_CAST")
-class DefaultDecoderRegistry(private val decoders: List<Decoder<*>>) : DecoderRegistry {
-
-  override fun <T : Any> decoder(t: KClass<T>): ConfigResult<Decoder<T>> {
-    return decoder(t.createType()).map { it as Decoder<T> }
-  }
-
-  override fun decoder(type: KType): ConfigResult<Decoder<*>> =
-    decoders.find { it.supports(type) }?.valid() ?: ConfigFailure.NoSuchDecoder(type, decoders).invalid()
-
-  override fun register(decoder: Decoder<*>): DecoderRegistry = DefaultDecoderRegistry(decoders + decoder)
-
-  override val size: Int = decoders.size
-}
-
-fun defaultDecoderRegistry(): DecoderRegistry {
-  return ServiceLoader.load(Decoder::class.java).toList()
-    .fold(DecoderRegistry.zero) { registry, decoder -> registry.register(decoder) }
-}
 
 /**
  * A typeclass for decoding a [Node] into a specified type.
---
+++ hoplite-core/src/main/kotlin/com/sksamuel/hoplite/decoder/DecoderRegistry.kt
@@ -0,0 +1,48 @@
+package com.sksamuel.hoplite.decoder
+
+import arrow.core.invalid
+import arrow.core.valid
+import com.sksamuel.hoplite.ConfigFailure
+import com.sksamuel.hoplite.ConfigResult
+import java.util.*
+import kotlin.reflect.KClass
+import kotlin.reflect.KType
+import kotlin.reflect.full.createType
+
+/**
+ * An immutable registry for providing instances of a [Decoder] for a given type.
+ */
+interface DecoderRegistry {
+
+  fun <T : Any> decoder(t: KClass<T>): ConfigResult<Decoder<T>>
+  fun decoder(type: KType): ConfigResult<Decoder<*>>
+  fun register(decoder: Decoder<*>): DecoderRegistry
+  val size: Int
+
+  companion object {
+    val zero: DecoderRegistry = DefaultDecoderRegistry(
+      emptyList())
+  }
+}
+
+inline fun <reified T : Any> DecoderRegistry.decoder(): ConfigResult<Decoder<T>> = decoder(T::class)
+
+@Suppress("UNCHECKED_CAST")
+class DefaultDecoderRegistry(private val decoders: List<Decoder<*>>) : DecoderRegistry {
+
+  override fun <T : Any> decoder(t: KClass<T>): ConfigResult<Decoder<T>> {
+    return decoder(t.createType()).map { it as Decoder<T> }
+  }
+
+  override fun decoder(type: KType): ConfigResult<Decoder<*>> =
+    decoders.find { it.supports(type) }?.valid() ?: ConfigFailure.NoSuchDecoder(type).invalid()
+
+  override fun register(decoder: Decoder<*>): DecoderRegistry = DefaultDecoderRegistry(decoders + decoder)
+
+  override val size: Int = decoders.size
+}
+
+fun defaultDecoderRegistry(): DecoderRegistry {
+  return ServiceLoader.load(Decoder::class.java).toList()
+    .fold(DecoderRegistry.zero) { registry, decoder -> registry.register(decoder) }
+}
