0347da9231acaab79e45646131906002de647578
sksamuel <sam@sksamuel.com>
2019-10-30 14:19:42 -0500

    Added KClass decoder

---
+++ hoplite-core/src/main/kotlin/com/sksamuel/hoplite/decoder/KClassDecoder.kt
@@ -0,0 +1,22 @@
+package com.sksamuel.hoplite.decoder
+
+import arrow.core.invalid
+import com.sksamuel.hoplite.ConfigFailure
+import com.sksamuel.hoplite.ConfigResult
+import com.sksamuel.hoplite.DecoderContext
+import com.sksamuel.hoplite.Node
+import com.sksamuel.hoplite.StringNode
+import com.sksamuel.hoplite.ThrowableFailure
+import kotlin.reflect.KClass
+import kotlin.reflect.KType
+
+class KClassDecoder : NonNullableDecoder<KClass<*>> {
+  override fun supports(type: KType): Boolean = type.classifier == KClass::class
+  override fun safeDecode(node: Node,
+                          type: KType,
+                          context: DecoderContext): ConfigResult<KClass<*>> = when (node) {
+    is StringNode -> kotlin.runCatching { Class.forName(node.value).kotlin }.toValidated { ThrowableFailure(it) }
+    else -> ConfigFailure.DecodeError(node, type).invalid()
+  }
+}
+
