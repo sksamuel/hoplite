0673649b997b787e9ce4ad44c5e2508d27f50f51
sksamuel <sam@sksamuel.com>
2019-11-08 15:14:00 -0600

    Added test for issue #72

---
+++ hoplite-yaml/src/test/kotlin/com/sksamuel/hoplite/yaml/GithubIssue72.kt
@@ -0,0 +1,34 @@
+package com.sksamuel.hoplite.yaml
+
+import com.sksamuel.hoplite.ConfigLoader
+import io.kotlintest.shouldBe
+import io.kotlintest.specs.StringSpec
+
+data class EngineConfig(
+  val env: String,
+  val redis: RedisConfig,
+  val odb: OdbConfig)
+
+data class RedisConfig(
+  val url: String,
+  val requestChannel: String,
+  val responseChannel: String)
+
+data class OdbConfig(
+  val home: String,
+  val configFile: String,
+  val dbFile: String,
+  val server: Boolean)
+
+class GithubIssue72 : StringSpec() {
+  init {
+    "NoSuchFieldError: Companion #72" {
+      ConfigLoader().loadConfigOrThrow<EngineConfig>("/github72.yml") shouldBe
+        EngineConfig(
+          env = "production",
+          redis = RedisConfig(url = "redis://localhost:6379/", requestChannel = "foo", responseChannel = "foo"),
+          odb = OdbConfig(home = "db", configFile = "config/production.conf", dbFile = "engine.db", server = false)
+        )
+    }
+  }
+}
