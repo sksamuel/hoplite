3ed0fdaf7917615f6a71c439e01483de89a71f26
Mathias Düsterhöft <mathias.duesterhoeft@moia.io>
2020-01-14 09:27:12 +0100

    Add default value handling for environment variable expressions.

--- hoplite-core/src/main/kotlin/com/sksamuel/hoplite/preprocessor/EnvVarPreprocessor.kt
+++ hoplite-core/src/main/kotlin/com/sksamuel/hoplite/preprocessor/EnvVarPreprocessor.kt
@@ -6,12 +6,17 @@ import com.sksamuel.hoplite.StringNode
 object EnvVarPreprocessor : Preprocessor {
 
   private val regex = "\\$\\{(.*?)}".toRegex()
+  private val valueWithDefaultRegex = "(.*?):-(.*?)".toRegex()
 
   override fun process(node: Node): Node = when (node) {
     is StringNode -> {
       val value = regex.replace(node.value) {
         val key = it.groupValues[1]
-        System.getenv(key) ?: it.value
+        when (val matchWithDefault = valueWithDefaultRegex.matchEntire(key)) {
+          null -> System.getenv(key) ?: it.value
+          // lookup with default value fallback
+          else -> matchWithDefault.let { m -> System.getenv(m.groups[1]!!.value) ?: m.groups[2]!!.value }
+        }
       }
       node.copy(value = value)
     }
