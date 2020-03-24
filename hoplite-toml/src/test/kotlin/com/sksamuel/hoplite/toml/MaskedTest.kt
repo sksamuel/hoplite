2fccc6d26ae13876ce27a3bb2ac33fc881ee4da4
sksamuel <sam@sksamuel.com>
2020-03-23 19:40:06 -0500

    Bumped kotest to 4.0.0 final

--- hoplite-toml/src/test/kotlin/com/sksamuel/hoplite/toml/MaskedTest.kt
+++ hoplite-toml/src/test/kotlin/com/sksamuel/hoplite/toml/MaskedTest.kt
@@ -10,7 +10,6 @@ class MaskedTest : StringSpec() {
   init {
     "support built in Masked type" {
       data class Test(val a: Masked)
-
       val test = ConfigLoader().loadConfigOrThrow<Test>("/masked.toml")
       test.a.shouldBeTypeOf<Masked>()
       test.a.value shouldBe "password"
