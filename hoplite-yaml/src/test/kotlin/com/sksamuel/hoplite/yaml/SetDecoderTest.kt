2cdaa8c1919a9a43c1f2996f08fe258d38c808b5
Gilles Barbier <gilles@zenaton.com>
2020-12-01 15:36:41 +0100

    add test for failing set of enum cases (#180)

--- hoplite-yaml/src/test/kotlin/com/sksamuel/hoplite/yaml/SetDecoderTest.kt
+++ hoplite-yaml/src/test/kotlin/com/sksamuel/hoplite/yaml/SetDecoderTest.kt
@@ -6,11 +6,18 @@ import io.kotest.matchers.shouldBe
 import io.kotest.core.spec.style.StringSpec
 
 class SetDecoderTest : StringSpec({
-  "file decoded from json" {
+  "Set decoded from yml file" {
     data class Test(val a: Set<Long>, val b: Set<String>)
 
     val config = ConfigLoader().loadConfigOrThrow<Test>("/sets.yml")
     config shouldBe Test(setOf(1, 2, 3), setOf("1", "2"))
     config.a.shouldBeInstanceOf<HashSet<*>>()
+  }
+
+  "Set of Kotlin.Enum decoded from yml file" {
+    data class TestEnum(val wines: Set<Wine>)
+
+    val config = ConfigLoader().loadConfigOrThrow<TestEnum>("/test_set_enum.yml")
+    config shouldBe TestEnum(setOf(Wine.Malbec, Wine.Shiraz))
   }
 })
