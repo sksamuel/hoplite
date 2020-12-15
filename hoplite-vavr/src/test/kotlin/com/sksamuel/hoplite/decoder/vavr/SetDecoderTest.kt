444c7c5b265ebe3a83418ce01b93b1a9b2c138f5
crejk <crejk@ctemplar.com>
2020-12-15 21:05:31 +0100

    fix SetDecoderTest

--- hoplite-vavr/src/test/kotlin/com/sksamuel/hoplite/decoder/vavr/SetDecoderTest.kt
+++ hoplite-vavr/src/test/kotlin/com/sksamuel/hoplite/decoder/vavr/SetDecoderTest.kt
@@ -3,16 +3,16 @@ package com.sksamuel.hoplite.decoder.vavr
 import com.sksamuel.hoplite.ConfigLoader
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
-import io.vavr.collection.List
-import io.vavr.kotlin.list
+import io.vavr.collection.Set
+import io.vavr.kotlin.linkedHashSet
 
 class SetDecoderTest : FunSpec({
 
   test("Set<Int> decoded from yaml") {
-    data class Test(val numbers: List<Int>)
+    data class Test(val numbers: Set<Int>)
 
     val config = ConfigLoader().loadConfigOrThrow<Test>("/test_set.yml")
-    config shouldBe Test(list(1, 2, 3))
+    config shouldBe Test(linkedHashSet(1, 2, 3))
   }
 
 })
