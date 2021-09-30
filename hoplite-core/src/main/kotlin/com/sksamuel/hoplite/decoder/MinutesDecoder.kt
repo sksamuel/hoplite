d02b0c37c53e646f6b54fb6f2276bcb7e3ad137c
sksamuel <sam@sksamuel.com>
2021-09-30 11:02:05 -0500

    Added seconds and minutes decoders

---
+++ hoplite-core/src/main/kotlin/com/sksamuel/hoplite/decoder/MinutesDecoder.kt
@@ -0,0 +1,29 @@
+package com.sksamuel.hoplite.decoder
+
+import com.sksamuel.hoplite.ConfigFailure
+import com.sksamuel.hoplite.ConfigResult
+import com.sksamuel.hoplite.DecoderContext
+import com.sksamuel.hoplite.LongNode
+import com.sksamuel.hoplite.Node
+import com.sksamuel.hoplite.StringNode
+import com.sksamuel.hoplite.ThrowableFailure
+import com.sksamuel.hoplite.fp.Try
+import com.sksamuel.hoplite.fp.invalid
+import com.sksamuel.hoplite.fp.valid
+import kotlin.reflect.KType
+
+class MinutesDecoder : NonNullableLeafDecoder<Minutes> {
+
+  override fun supports(type: KType): Boolean = type.classifier == Minutes::class
+  override fun safeLeafDecode(
+    node: Node,
+    type: KType,
+    context: DecoderContext
+  ): ConfigResult<Minutes> = when (node) {
+    is StringNode -> Try { Minutes(node.value.toLong()) }.toValidated { ThrowableFailure(it) }
+    is LongNode -> Minutes(node.value).valid()
+    else -> ConfigFailure.DecodeError(node, type).invalid()
+  }
+}
+
+data class Minutes(val value: Long)
---
+++ hoplite-core/src/main/kotlin/com/sksamuel/hoplite/decoder/SecondsDecoder.kt
@@ -0,0 +1,29 @@
+package com.sksamuel.hoplite.decoder
+
+import com.sksamuel.hoplite.ConfigFailure
+import com.sksamuel.hoplite.ConfigResult
+import com.sksamuel.hoplite.DecoderContext
+import com.sksamuel.hoplite.LongNode
+import com.sksamuel.hoplite.Node
+import com.sksamuel.hoplite.StringNode
+import com.sksamuel.hoplite.ThrowableFailure
+import com.sksamuel.hoplite.fp.Try
+import com.sksamuel.hoplite.fp.invalid
+import com.sksamuel.hoplite.fp.valid
+import kotlin.reflect.KType
+
+class SecondsDecoder : NonNullableLeafDecoder<Seconds> {
+
+  override fun supports(type: KType): Boolean = type.classifier == Seconds::class
+  override fun safeLeafDecode(
+    node: Node,
+    type: KType,
+    context: DecoderContext
+  ): ConfigResult<Seconds> = when (node) {
+    is StringNode -> Try { Seconds(node.value.toLong()) }.toValidated { ThrowableFailure(it) }
+    is LongNode -> Seconds(node.value).valid()
+    else -> ConfigFailure.DecodeError(node, type).invalid()
+  }
+}
+
+data class Seconds(val value: Long)
