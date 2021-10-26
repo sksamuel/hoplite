8a6216d5a316e2255b16a4fe7a31e4f016616cfe
Alexis Sawenko <alexis@airwallex.com>
2021-10-26 11:52:34 +1100

    Issue 230 fix WithoutDefaultsRegistryTest auto inject env var on "PATH" when env var is "Path"

--- hoplite-core/src/test/kotlin/com/sksamuel/hoplite/WithoutDefaultsRegistryTest.kt
+++ hoplite-core/src/test/kotlin/com/sksamuel/hoplite/WithoutDefaultsRegistryTest.kt
@@ -10,11 +10,11 @@ class WithoutDefaultsRegistryTest : FunSpec() {
   init {
     test("default registry throws no error") {
       val loader = ConfigLoader {
-        addMapSource(mapOf("custom_value" to "\${PATH}"))
+        addMapSource(mapOf("custom_value" to "\${PATH}", "PATH" to "\${PATH}"))
       }
       val e = loader.loadConfig<Config>()
-      e as Validated.Invalid<ConfigFailure>
-      e.error shouldBe instanceOf(ConfigFailure.DataClassFieldErrors::class)
+      e as Validated.Valid<Config>
+      e.value.customValue shouldNotBe "\${path}"
     }
 
     test("empty sources registry throws error") {
@@ -41,14 +41,14 @@ class WithoutDefaultsRegistryTest : FunSpec() {
     test("empty preprocessors registry throws error") {
       val loader = ConfigLoader {
         withDefaultPreprocessors(false)
-        addMapSource(mapOf("custom_value" to "\${PATH}"))
+        addMapSource(mapOf("custom_value" to "\${PATH}", "PATH" to "\${PATH}"))
       }
       val e = loader.loadConfig<Config>()
-      e as Validated.Invalid<ConfigFailure>
-      e.error shouldBe instanceOf(ConfigFailure.DataClassFieldErrors::class)
+      e as Validated.Valid<Config>
+      e.value.customValue shouldBe "\${PATH}"
     }
 
   }
-
+   // if your env vars is not "PATH" and is "Path" auto inject doesn't work
   data class Config(val PATH: String, val customValue: String)
 }
