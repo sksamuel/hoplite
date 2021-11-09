6316d1b32bd914615261e5c5075ceaddd5cae3c9
sksamuel <sam@sksamuel.com>
2021-11-09 09:27:26 -0600

    Added ParameterStorePathPreprocessor

---
+++ hoplite-aws/src/main/kotlin/com/sksamuel/hoplite/aws/ParameterStorePathPreprocessor.kt
@@ -0,0 +1,50 @@
+package com.sksamuel.hoplite.aws
+
+import com.amazonaws.services.simplesystemsmanagement.AWSSimpleSystemsManagementClientBuilder
+import com.amazonaws.services.simplesystemsmanagement.model.GetParametersByPathRequest
+import com.amazonaws.services.simplesystemsmanagement.model.Parameter
+import com.sksamuel.hoplite.ConfigException
+import com.sksamuel.hoplite.Node
+import com.sksamuel.hoplite.PrimitiveNode
+import com.sksamuel.hoplite.StringNode
+import com.sksamuel.hoplite.preprocessor.TraversingPrimitivePreprocessor
+
+/**
+ * Loads all config keys under the given path and then applies them to the tree.
+ *
+ * Note: If the values are encrypted, you must enable withDecryption inside the configure block.
+ *
+ * @param path the path to use.
+ * @param configure the AWS request object for custom configuration. Optional.
+ *
+ */
+class ParameterStorePathPreprocessor(
+  private val path: String,
+  private val configure: GetParametersByPathRequest.() -> Unit
+) : TraversingPrimitivePreprocessor() {
+
+  private val client by lazy { AWSSimpleSystemsManagementClientBuilder.defaultClient() }
+  private val regex = "\\$\\{ssm:(.+?)}".toRegex()
+
+  private fun fetchParameterStoreValues(): Result<List<Parameter>> = runCatching {
+    val req = GetParametersByPathRequest().withPath(path).apply(configure)
+    client.getParametersByPath(req).parameters
+  }
+
+  override fun handle(node: PrimitiveNode): Node = when (node) {
+    is StringNode -> {
+      when (val match = regex.matchEntire(node.value)) {
+        null -> node
+        else -> {
+          val key = match.groupValues[1]
+          val values = fetchParameterStoreValues().getOrElse { throw ConfigException("Failed to load parameters", it) }
+          val value = values
+            .firstOrNull { it.name == key }
+            ?: throw ConfigException("Could not find path: $path in paths: ${values.map { it.name }}")
+          node.copy(value = value.value)
+        }
+      }
+    }
+    else -> node
+  }
+}
