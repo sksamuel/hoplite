aab27745a5a5e2736864132c6f574307e14226d5
Jan Březina <brezina.jn@gmail.com>
2021-11-10 17:22:22 +0100

    add LocalTimeDecoder

---
+++ hoplite-datetime/src/test/kotlin/com/sksamuel/hoplite/datetime/LocalTimeDecoderTest.kt
@@ -0,0 +1,18 @@
+package com.sksamuel.hoplite.datetime
+
+import com.sksamuel.hoplite.ConfigLoader
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+import kotlinx.datetime.LocalDate
+import java.time.LocalTime
+
+class LocalTimeDecoderTest : FunSpec() {
+  init {
+    test("test local time") {
+      data class Config(val a: LocalTime, val b: LocalTime)
+      val config = ConfigLoader().loadConfigOrThrow<Config>("/localtime.props")
+      config.a shouldBe LocalTime.parse("18:15")
+      config.b shouldBe LocalTime.parse("18:15:59")
+    }
+  }
+}
