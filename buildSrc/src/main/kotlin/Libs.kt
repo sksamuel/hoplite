174930340fc07c492014a3245a23a062f551a363
Michael Bailey <1195562+yogurtearl@users.noreply.github.com>
2022-03-16 17:42:59 -0700

    Update to Kotlin 1.6.10 (#267)
    * The versioning of kotlinx-coroutines-core artifact is separate from kotlin.
    
    recent versions of kotlinx-coroutines-core:
    1.5.0, 1.5.1, 1.5.2, 1.6.0 (latest)
    
    recent versions of kotlin:
    1.5.30, 1.5.31, 1.5.32, 1.6.0, 1.6.10 (latest)
    
    * Update to kotlin 1.6
    
    related comment:
    https://github.com/sksamuel/hoplite/issues/226#issuecomment-1069678764

--- buildSrc/src/main/kotlin/Libs.kt
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -1,13 +1,13 @@
 object Libs {
 
-  const val kotlinVersion = "1.5.0"
+  const val kotlinVersion = "1.6.10"
   const val org = "com.sksamuel.hoplite"
 
   object Kotlin {
     const val reflect = "org.jetbrains.kotlin:kotlin-reflect:$kotlinVersion"
     const val stdlib = "org.jetbrains.kotlin:kotlin-stdlib:$kotlinVersion"
-    const val datetime = "org.jetbrains.kotlinx:kotlinx-datetime:0.2.0"
-    const val coroutines = "org.jetbrains.kotlinx:kotlinx-coroutines-core:$kotlinVersion"
+    const val datetime = "org.jetbrains.kotlinx:kotlinx-datetime:0.3.2"
+    const val coroutines = "org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0"
   }
 
   object Arrow {
