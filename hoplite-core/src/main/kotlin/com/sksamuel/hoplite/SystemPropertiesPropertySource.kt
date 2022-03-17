a676dc00863654464b97f8e6405d8477f320bc29
Michael Bailey <1195562+yogurtearl@users.noreply.github.com>
2022-03-17 16:46:56 -0700

    Make SystemPropertiesPropertySource easier to test (#270)
    Calling System.getProperties() directly makes this hard to test
    because it involved global mutable state and you have to
    remember to `clearProperty` whenever you set the property.
    
    This allows you to just pass a map directly for system properties.

--- hoplite-core/src/main/kotlin/com/sksamuel/hoplite/SystemPropertiesPropertySource.kt
+++ hoplite-core/src/main/kotlin/com/sksamuel/hoplite/SystemPropertiesPropertySource.kt
@@ -10,14 +10,31 @@ import java.util.Properties
  * In other words, if a System property is defined 'config.override.user.name=sam' then
  * the property 'user.name=sam' is made available.
  */
-object SystemPropertiesPropertySource : PropertySource {
-  private const val prefix = "config.override."
+open class SystemPropertiesPropertySource(
+  private val systemPropertiesMap: () -> Map<String, String> = { System.getProperties().toStringMap() }
+) : PropertySource {
   override fun node(context: PropertySourceContext): ConfigResult<Node> {
     val props = Properties()
-    System.getProperties()
-      .stringPropertyNames()
-      .filter { it.startsWith(prefix) }
-      .forEach { props[it.removePrefix(prefix)] = System.getProperty(it) }
+    systemPropertiesMap().let { systemPropertiesMap ->
+      systemPropertiesMap.keys
+        .filter { it.startsWith(prefix) }
+        .forEach { props[it.removePrefix(prefix)] = systemPropertiesMap[it] }
+    }
     return if (props.isEmpty) Undefined.valid() else props.toNode("sysprops").valid()
+  }
+
+  companion object : SystemPropertiesPropertySource() {
+    private const val prefix = "config.override."
+
+    // for backwards compatibility with Java from when SystemPropertiesPropertySource was an `object`
+    @Suppress("unused")
+    @JvmField
+    val INSTANCE: SystemPropertiesPropertySource = this
+  }
+}
+
+internal fun Properties.toStringMap(): Map<String, String> = this.let { systemProperties ->
+  systemProperties.stringPropertyNames().associateWith { propertyName ->
+    systemProperties.getProperty(propertyName)
   }
 }
