74ae85456fb04eb24937d6f73471297b6a3c269f
Sam Sam <sam@sksamuel.com>
2022-03-17 19:32:02 -0500

    Updated to java 11 (#273)

--- build.gradle.kts
+++ build.gradle.kts
@@ -44,7 +44,7 @@ allprojects {
    }
 
    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-      kotlinOptions.jvmTarget = "1.8"
+      kotlinOptions.jvmTarget = "11"
    }
 
    java {
