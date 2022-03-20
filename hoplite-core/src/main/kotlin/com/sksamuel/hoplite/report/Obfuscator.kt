148892d40425d316b541f453c45c05ffa9e92034
sksamuel <sam@sksamuel.com>
2022-03-20 14:25:59 -0500

    Updated hash obfuscator

--- hoplite-core/src/main/kotlin/com/sksamuel/hoplite/report/Obfuscator.kt
+++ hoplite-core/src/main/kotlin/com/sksamuel/hoplite/report/Obfuscator.kt
@@ -1,6 +1,7 @@
 package com.sksamuel.hoplite.report
 
 import com.sksamuel.hoplite.decoder.DotPath
+import java.security.MessageDigest
 
 /**
  * Implementations can choose how to obfuscate values.
@@ -13,8 +14,14 @@ object StrictObfuscator : Obfuscator {
   override fun obfuscate(path: DotPath, value: String): String = "*****"
 }
 
+/**
+ * Obfuscates values by taking the first 8 characters of the SHA-256 hash of the input value.
+ */
 object HashObfuscator : Obfuscator {
-  override fun obfuscate(path: DotPath, value: String): String = value.hashCode().toString()
+  override fun obfuscate(path: DotPath, value: String): String {
+    val digest = MessageDigest.getInstance("SHA-256")
+    return digest.digest(value.encodeToByteArray()).decodeToString().take(8)
+  }
 }
 
 /**
