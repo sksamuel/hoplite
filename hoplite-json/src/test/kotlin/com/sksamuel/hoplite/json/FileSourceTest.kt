387f3560598a4b2ab546bb8ab72c26a12d895095
sksamuel <sam@sksamuel.com>
2022-03-23 12:40:34 -0500

    updated tests

--- hoplite-json/src/test/kotlin/com/sksamuel/hoplite/json/FileSourceTest.kt
+++ hoplite-json/src/test/kotlin/com/sksamuel/hoplite/json/FileSourceTest.kt
@@ -2,6 +2,7 @@ package com.sksamuel.hoplite.json
 
 import com.sksamuel.hoplite.ConfigLoaderBuilder
 import com.sksamuel.hoplite.PropertySource
+import com.sksamuel.hoplite.addEnvironmentSource
 import io.kotest.assertions.throwables.shouldThrowAny
 import io.kotest.core.spec.style.FunSpec
 import java.io.File
@@ -10,12 +11,17 @@ class FileSourceTest : FunSpec() {
   init {
 
     test("PropertySource.file should should not fail for optionals") {
-      ConfigLoaderBuilder.default().addSource(PropertySource.file(File("wibble"), true)).build().loadNodeOrThrow()
+      ConfigLoaderBuilder.default()
+        .addEnvironmentSource() // so we have at least one value
+        .addSource(PropertySource.file(File("wibble"), true)).build().loadNodeOrThrow()
     }
 
     test("PropertySource.file should should fail for required file that does not exist") {
       shouldThrowAny {
-        ConfigLoaderBuilder.default().addSource(PropertySource.file(File("wibble"), false)).build().loadNodeOrThrow()
+        ConfigLoaderBuilder.default()
+          .addEnvironmentSource() // so we have at least one value
+          .addSource(PropertySource.file(File("wibble"), false))
+          .build().loadNodeOrThrow()
       }
     }
   }
