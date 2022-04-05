42dd1e393ae87923621875a2c106083f2c02ee45
sksamuel <sam@sksamuel.com>
2022-04-05 01:09:45 -0500

    Fixed test

--- hoplite-core/src/test/kotlin/com/sksamuel/hoplite/EmptyDecoderRegistryTest.kt
+++ hoplite-core/src/test/kotlin/com/sksamuel/hoplite/EmptyDecoderRegistryTest.kt
@@ -21,7 +21,7 @@ class EmptyDecoderRegistryTest : FunSpec() {
         preprocessors,
         mappers,
         allowEmptyTree = false,
-        allowUnresolvedSubstitutions = allowUnresolvedSubstitutions
+        allowUnresolvedSubstitutions = false,
       ).loadConfig<Config>()
       e as Validated.Invalid<ConfigFailure>
       e.error shouldBe ConfigFailure.EmptyDecoderRegistry
