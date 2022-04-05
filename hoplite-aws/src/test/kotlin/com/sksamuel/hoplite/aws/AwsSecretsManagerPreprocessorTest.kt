391b1d580cb8af21678762c547bf3f59d38e2413
sksamuel <sam@sksamuel.com>
2022-04-05 01:10:00 -0500

    Fixed test

--- hoplite-aws/src/test/kotlin/com/sksamuel/hoplite/aws/AwsSecretsManagerPreprocessorTest.kt
+++ hoplite-aws/src/test/kotlin/com/sksamuel/hoplite/aws/AwsSecretsManagerPreprocessorTest.kt
@@ -21,48 +21,48 @@ import org.testcontainers.containers.localstack.LocalStackContainer
 import org.testcontainers.utility.DockerImageName
 
 class AwsSecretsManagerPreprocessorTest : FunSpec() {
-//
-//  private val localstackImage = DockerImageName.parse("localstack/localstack:0.14.2")
-//  private val localstack = LocalStackContainer(localstackImage).withServices(LocalStackContainer.Service.SECRETSMANAGER)
+
+  private val localstackImage = DockerImageName.parse("localstack/localstack:0.14.2")
+  private val localstack = LocalStackContainer(localstackImage).withServices(LocalStackContainer.Service.SECRETSMANAGER)
 
   init {
 
-//    install(TestContainerExtension(localstack))
-//
-//    val client = AWSSecretsManagerClientBuilder.standard()
-//      .withEndpointConfiguration(localstack.getEndpointConfiguration(LocalStackContainer.Service.SECRETSMANAGER))
-//      .withCredentials(localstack.defaultCredentialsProvider)
-//      .build()
+    install(TestContainerExtension(localstack))
 
-    test("placeholder should be detected and used") {
+    val client = AWSSecretsManagerClientBuilder.standard()
+      .withEndpointConfiguration(localstack.getEndpointConfiguration(LocalStackContainer.Service.SECRETSMANAGER))
+      .withCredentials(localstack.defaultCredentialsProvider)
+      .build()
 
-//      client.createSecret(CreateSecretRequest().withName("foo").withSecretString("secret!"))
+    test("!placeholder should be detected and used") {
+
+      client.createSecret(CreateSecretRequest().withName("foo").withSecretString("secret!"))
 
       ConfigLoaderBuilder.default()
-        .addPreprocessor(AwsSecretsManagerPreprocessor())
+        .addPreprocessor(AwsSecretsManagerPreprocessor { client })
         .build()
         .loadConfigOrThrow<ConfigHolder>("/secrets.props")
         .a.shouldBe("secret!")
     }
 
-//    test("unknown secret should return error and include key") {
-//      AwsSecretsManagerPreprocessor { client }.process(StringNode("secretsmanager://unkunk", Pos.NoPos, DotPath.root))
-//        .shouldBeInstanceOf<Validated.Invalid<ConfigFailure>>().error.description().shouldContain("unkunk")
-//    }
-//
-//    test("unknown secret should return error and not include prefix") {
-//      AwsSecretsManagerPreprocessor { client }.process(StringNode("secretsmanager://unkunk", Pos.NoPos, DotPath.root))
-//        .shouldBeInstanceOf<Validated.Invalid<ConfigFailure>>().error.description().shouldNotContain("secretsmanager://")
-//    }
-//
-//    test("multiple errors should be returned at once") {
-//      shouldThrow<ConfigException> {
-//        ConfigLoaderBuilder.default()
-//          .addPreprocessor(AwsSecretsManagerPreprocessor { client })
-//          .build()
-//          .loadConfigOrThrow<ConfigHolder>("/multiple_secrets.props")
-//      }.message.shouldContain("foo.bar").shouldContain("bar.baz")
-//    }
+    test("unknown secret should return error and include key") {
+      AwsSecretsManagerPreprocessor { client }.process(StringNode("secretsmanager://unkunk", Pos.NoPos, DotPath.root))
+        .shouldBeInstanceOf<Validated.Invalid<ConfigFailure>>().error.description().shouldContain("unkunk")
+    }
+
+    test("unknown secret should return error and not include prefix") {
+      AwsSecretsManagerPreprocessor { client }.process(StringNode("secretsmanager://unkunk", Pos.NoPos, DotPath.root))
+        .shouldBeInstanceOf<Validated.Invalid<ConfigFailure>>().error.description().shouldNotContain("secretsmanager://")
+    }
+
+    test("multiple errors should be returned at once") {
+      shouldThrow<ConfigException> {
+        ConfigLoaderBuilder.default()
+          .addPreprocessor(AwsSecretsManagerPreprocessor { client })
+          .build()
+          .loadConfigOrThrow<ConfigHolder>("/multiple_secrets.props")
+      }.message.shouldContain("foo.bar").shouldContain("bar.baz")
+    }
   }
 
   data class ConfigHolder(val a: String)
