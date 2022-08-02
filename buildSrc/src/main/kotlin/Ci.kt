f7b3e53ae04abddcafa08f1f228e8a5423b7f1ee
sksamuel <sam@sksamuel.com>
2022-08-01 23:40:21 -0500

    Set next snapshot base to 2.6.0

--- buildSrc/src/main/kotlin/Ci.kt
+++ buildSrc/src/main/kotlin/Ci.kt
@@ -2,7 +2,7 @@ object Ci {
 
    // this is the version used for building snapshots
    // .GITHUB_RUN_NUMBER-snapshot will be appended
-   private const val snapshotBase = "2.5.0"
+   private const val snapshotBase = "2.6.0"
 
    private val githubRunNumber = System.getenv("GITHUB_RUN_NUMBER")
 
