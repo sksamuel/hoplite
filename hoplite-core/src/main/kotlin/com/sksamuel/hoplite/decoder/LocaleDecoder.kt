47e5b242563a8989f1ccbf3c1c617097b0458878
sksamuel <sam@sksamuel.com>
2022-08-13 21:09:56 -0500

    Added java.util.Locale decoder

---
+++ hoplite-core/src/main/kotlin/com/sksamuel/hoplite/decoder/LocaleDecoder.kt
@@ -0,0 +1,25 @@
+package com.sksamuel.hoplite.decoder
+
+import com.sksamuel.hoplite.ConfigFailure
+import com.sksamuel.hoplite.ConfigResult
+import com.sksamuel.hoplite.DecoderContext
+import com.sksamuel.hoplite.Node
+import com.sksamuel.hoplite.StringNode
+import com.sksamuel.hoplite.fp.invalid
+import com.sksamuel.hoplite.fp.valid
+import java.util.Locale
+import kotlin.reflect.KType
+
+class LocaleDecoder : NullHandlingDecoder<Locale> {
+
+  override fun supports(type: KType): Boolean = type.classifier == Locale::class
+
+  override fun safeDecode(
+    node: Node,
+    type: KType,
+    context: DecoderContext
+  ): ConfigResult<Locale> = when (node) {
+    is StringNode -> Locale.forLanguageTag(node.value).valid()
+    else -> ConfigFailure.DecodeError(node, type).invalid()
+  }
+}
