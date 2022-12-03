505956ddb8269c40a33472af3e7507d634ccf049
ArtificialPB <matej@kriptal.io>
2022-12-03 16:10:06 +0100

    Use shaded TomlJ library to avoid ANTLR version conflicts

--- hoplite-toml/build.gradle.kts
+++ hoplite-toml/build.gradle.kts
@@ -1,6 +1,11 @@
 dependencies {
    api(projects.hopliteCore)
-   implementation(libs.tomlj)
+
+   implementation(libs.tomlj) {
+      artifact {
+         classifier = "all"
+      }
+   }
 }
 
 apply("../publish.gradle.kts")
