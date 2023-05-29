3b56046ebf47a98a7c4d475d12a137754805755f
sksamuel <sam@sksamuel.com>
2023-05-29 13:30:31 -0500

    Add hostname validator #373

---
+++ hoplite-core/src/main/kotlin/com/sksamuel/hoplite/resolver/validator/HostnameValidator.kt
@@ -0,0 +1,34 @@
+package com.sksamuel.hoplite.resolver.validator
+
+import com.sksamuel.hoplite.ConfigFailure
+import com.sksamuel.hoplite.ConfigResult
+import com.sksamuel.hoplite.DecoderContext
+import com.sksamuel.hoplite.Node
+import com.sksamuel.hoplite.StringNode
+import com.sksamuel.hoplite.fp.invalid
+import com.sksamuel.hoplite.fp.valid
+import com.sksamuel.hoplite.resolver.Resolver
+import java.net.InetAddress
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.seconds
+
+class HostnameValidator(private val timeout: Duration = 5.seconds) : Resolver {
+
+  override suspend fun resolve(
+    paramName: String?,
+    node: Node,
+    root: Node,
+    context: DecoderContext
+  ): ConfigResult<Node> {
+    return if (node is StringNode && (paramName == "hostname" || paramName == "host")) {
+      val hostname = node.value
+      val failure = ConfigFailure.ValidationFailed("Hostname `$hostname` not reachable", node).invalid()
+      runCatching {
+        val reachable = InetAddress.getByName(hostname).isReachable(timeout.inWholeMilliseconds.toInt())
+        if (reachable) node.valid() else failure
+      }.getOrElse { failure }
+    } else {
+      node.valid()
+    }
+  }
+}
---
+++ hoplite-core/src/test/kotlin/com/sksamuel/hoplite/resolver/validator/HostnameValidatorTest.kt
@@ -0,0 +1,47 @@
+package com.sksamuel.hoplite.resolver.validator
+
+import com.sksamuel.hoplite.ConfigLoaderBuilder
+import com.sksamuel.hoplite.ExperimentalHoplite
+import com.sksamuel.hoplite.parsers.PropsPropertySource
+import io.kotest.assertions.throwables.shouldThrowAny
+import io.kotest.core.extensions.install
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.extensions.testcontainers.JdbcTestContainerExtension
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.string.shouldInclude
+import java.util.Properties
+
+@OptIn(ExperimentalHoplite::class)
+class HostnameValidatorTest : FunSpec({
+
+  install(JdbcTestContainerExtension(postgreSQLContainer))
+  data class Config(val hostname: String)
+
+  test("hostname validation for valid host") {
+
+    val props = Properties()
+    props["hostname"] = postgreSQLContainer.host
+
+    val config = ConfigLoaderBuilder.newBuilder()
+      .addPropertySource(PropsPropertySource(props))
+      .addResolver(HostnameValidator())
+      .build()
+      .loadConfigOrThrow<Config>()
+
+    config.hostname shouldBe postgreSQLContainer.host
+  }
+
+  test("hostname validation for invalid host") {
+
+    val props = Properties()
+    props["hostname"] = "invalidhostname"
+
+    shouldThrowAny {
+      ConfigLoaderBuilder.newBuilder()
+        .addPropertySource(PropsPropertySource(props))
+        .addResolver(HostnameValidator())
+        .build()
+        .loadConfigOrThrow<Config>()
+    }.message shouldInclude "Hostname `invalidhostname` not reachable at (props)"
+  }
+})
