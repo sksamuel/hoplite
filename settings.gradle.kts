c2077263400f764b561b0ac50ba1d56c6c9cf2e7
sksamuel <sam@sksamuel.com>
2023-08-06 12:00:13 -0500

    Updated versions

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -72,8 +72,8 @@ dependencyResolutionManagement {
 
          library("hikaricp", "com.zaxxer:HikariCP:5.0.1")
 
-         library("kotlinx-serialization-json", "org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
-         library("kotlinx-datetime", "org.jetbrains.kotlinx:kotlinx-datetime:0.3.3")
+         library("kotlinx-serialization-json", "org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
+         library("kotlinx-datetime", "org.jetbrains.kotlinx:kotlinx-datetime:0.4.0")
 
          library("google-cloud-secretmanager", "com.google.cloud:google-cloud-secretmanager:2.20.0")
          library("hadoop-common", "org.apache.hadoop:hadoop-common:2.10.2")
