61e440416a1bfe73fe52edc2e11da16c61a1053a
sksamuel <sam@sksamuel.com>
2023-10-15 19:56:27 -0500

    bumped versions

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -48,7 +48,7 @@ dependencyResolutionManagement {
 
          library("kotlin-reflect", "org.jetbrains.kotlin:kotlin-reflect:1.6.21")
 
-         val micrometer = "1.11.2"
+         val micrometer = "1.11.5"
          library("micrometer-core", "io.micrometer:micrometer-core:$micrometer")
          library("micrometer-registry-datadog", "io.micrometer:micrometer-registry-datadog:$micrometer")
          library("micrometer-registry-prometheus", "io.micrometer:micrometer-registry-prometheus:$micrometer")
@@ -62,11 +62,11 @@ dependencyResolutionManagement {
 
          library("arrow-core", "io.arrow-kt:arrow-core:1.1.3")
 
-         library("spring-vault-core", "org.springframework.vault:spring-vault-core:2.3.2")
+         library("spring-vault-core", "org.springframework.vault:spring-vault-core:2.3.4")
 
          library("vavr-kotlin", "io.vavr:vavr-kotlin:0.10.2")
 
-         val aws1 = "1.12.523"
+         val aws1 = "1.12.566"
          library("aws-java-sdk-secretsmanager", "com.amazonaws:aws-java-sdk-secretsmanager:$aws1")
          library("aws-java-sdk-ssm", "com.amazonaws:aws-java-sdk-ssm:$aws1")
 
@@ -77,22 +77,22 @@ dependencyResolutionManagement {
          library("kotlinx-serialization-json", "org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
          library("kotlinx-datetime", "org.jetbrains.kotlinx:kotlinx-datetime:0.4.0")
 
-         library("google-cloud-secretmanager", "com.google.cloud:google-cloud-secretmanager:2.20.0")
+         library("google-cloud-secretmanager", "com.google.cloud:google-cloud-secretmanager:2.28.0")
          library("hadoop-common", "org.apache.hadoop:hadoop-common:2.10.2")
          library("tomlj", "org.tomlj:tomlj:1.1.0")
          library("embedded-consul", "com.pszymczyk.consul:embedded-consul:2.2.1")
-         library("snakeyaml", "org.yaml:snakeyaml:2.1")
+         library("snakeyaml", "org.yaml:snakeyaml:2.2")
          library("kaml", "com.charleskorn.kaml:kaml:0.42.0")
 
-         library("slf4j-api", "org.slf4j:slf4j-api:2.0.7")
+         library("slf4j-api", "org.slf4j:slf4j-api:2.0.9")
 
-         val aws2 = "2.20.98"
+         val aws2 = "2.21.0"
          library("regions", "software.amazon.awssdk:regions:$aws2")
          library("secretsmanager", "software.amazon.awssdk:secretsmanager:$aws2")
 
          library("postgresql", "org.postgresql:postgresql:42.6.0")
 
-         val jackson = "2.15.2"
+         val jackson = "2.15.3"
          library("jackson-core", "com.fasterxml.jackson.core:jackson-core:$jackson")
          library("jackson-databind", "com.fasterxml.jackson.core:jackson-databind:$jackson")
 
@@ -102,8 +102,8 @@ dependencyResolutionManagement {
          library("testcontainers-localstack", "org.testcontainers:localstack:$testcontainers")
          library("testcontainers-vault", "org.testcontainers:vault:$testcontainers")
 
-         library("azure-identity", "com.azure:azure-identity:1.9.2")
-         library("azure-security-keyvault-secrets", "com.azure:azure-security-keyvault-secrets:4.6.4")
+         library("azure-identity", "com.azure:azure-identity:1.10.3")
+         library("azure-security-keyvault-secrets", "com.azure:azure-security-keyvault-secrets:4.7.0")
 
          library("consul-client", "com.orbitz.consul:consul-client:1.5.3")
       }
