4515bfdfa6f574b581b9e66a5719d12d4e6d5c19
shorinami <shorinami@atombuilt.com>
2024-03-11 05:09:05 +0200

    build: Change jvm target to 1.8 (Java 8)

--- build.gradle.kts
+++ build.gradle.kts
@@ -42,7 +42,7 @@ allprojects {
    }
 
    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-      kotlinOptions.jvmTarget = "11"
+      kotlinOptions.jvmTarget = "1.8"
    }
 
    java {
