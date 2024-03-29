95f498887450a75b1f977e5fd704c21423e1eacd
Raman Gupta <rocketraman@gmail.com>
2024-03-29 12:42:13 -0400

    Support an idiomatic environment source
    Resolves https://github.com/sksamuel/hoplite/issues/410.

--- hoplite-core/src/test/kotlin/com/sksamuel/hoplite/EnvironmentVariablesPropertySourceTest.kt
+++ hoplite-core/src/test/kotlin/com/sksamuel/hoplite/EnvironmentVariablesPropertySourceTest.kt
@@ -7,11 +7,12 @@ import io.kotest.matchers.shouldBe
 
 class EnvironmentVariablesPropertySourceTest : FunSpec({
 
-  test("build env source should include paths") {
+  test("build env source with the classic env var mapping should include paths") {
     EnvironmentVariablesPropertySource(
-      useUnderscoresAsSeparator = false,
+      useUnderscoresAsSeparator = true,
+      useSingleUnderscoresAsSeparator = false,
       allowUppercaseNames = false,
-      environmentVariableMap = { mapOf("a" to "foo", "a.b" to "bar", "c" to "baz") }
+      environmentVariableMap = { mapOf("a" to "foo", "a.b" to "bar", "c" to "baz", "d__e" to "gaz") }
     ).node(
       PropertySourceContext.empty
     ).getUnsafe() shouldBe MapNode(
@@ -24,6 +25,44 @@ class EnvironmentVariablesPropertySourceTest : FunSpec({
           sourceKey = "a"
         ),
         "c" to StringNode("baz", Pos.env, DotPath("c"), sourceKey = "c"),
+        "d" to MapNode(
+          value = Undefined,
+          map = mapOf("e" to StringNode("gaz", Pos.env, DotPath("d", "e"), sourceKey = "d__e")),
+          pos = Pos.SourcePos("env"),
+          path = DotPath("d"),
+          sourceKey = "d"
+        ),
+      ),
+      pos = Pos.env,
+      DotPath.root
+    )
+  }
+
+  test("build env source with the idiomatic env var mapping should include paths") {
+    EnvironmentVariablesPropertySource(
+      useUnderscoresAsSeparator = false,
+      useSingleUnderscoresAsSeparator = true,
+      allowUppercaseNames = false,
+      environmentVariableMap = { mapOf("a" to "foo", "a.b" to "bar", "c" to "baz", "d_e" to "gaz") }
+    ).node(
+      PropertySourceContext.empty
+    ).getUnsafe() shouldBe MapNode(
+      mapOf(
+        "a" to MapNode(
+          value = StringNode("foo", Pos.env, DotPath("a"), sourceKey = "a"),
+          map = mapOf("b" to StringNode("bar", Pos.env, DotPath("a", "b"), sourceKey = "a.b")),
+          pos = Pos.SourcePos("env"),
+          path = DotPath("a"),
+          sourceKey = "a"
+        ),
+        "c" to StringNode("baz", Pos.env, DotPath("c"), sourceKey = "c"),
+        "d" to MapNode(
+          value = Undefined,
+          map = mapOf("e" to StringNode("gaz", Pos.env, DotPath("d", "e"), sourceKey = "d_e")),
+          pos = Pos.SourcePos("env"),
+          path = DotPath("d"),
+          sourceKey = "d"
+        ),
       ),
       pos = Pos.env,
       DotPath.root
