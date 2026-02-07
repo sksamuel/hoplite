e66bceefa9630a95b50a7c49105dfc0d39732465
TheBestPessimist <cristian@tbp.land>
2026-02-07 15:40:18 +0200

    the fix? (with duplicated logic)

--- hoplite-core/src/main/kotlin/com/sksamuel/hoplite/ClasspathResourceLoader.kt
+++ hoplite-core/src/main/kotlin/com/sksamuel/hoplite/ClasspathResourceLoader.kt
@@ -18,10 +18,8 @@ interface ClasspathResourceLoader {
   fun getResourceAsStream(name: String): InputStream?
 
   companion object {
-    fun <T> Class<T>.toClasspathResourceLoader() = object : ClasspathResourceLoader {
-      override fun getResource(name: String) = this@toClasspathResourceLoader.getResource(name)
-      override fun getResourceAsStream(name: String) = this@toClasspathResourceLoader.getResourceAsStream(name)
-    }
+    // this one simply delegates to the [ClassLoader.toClasspathResourceLoader()] object below
+    fun <T> Class<T>.toClasspathResourceLoader() = this.classLoader.toClasspathResourceLoader()
 
     fun ClassLoader.toClasspathResourceLoader() = object : ClasspathResourceLoader {
       override fun getResource(name: String) = this@toClasspathResourceLoader.getResource(name)
@@ -29,5 +27,3 @@ interface ClasspathResourceLoader {
     }
   }
 }
-
-
